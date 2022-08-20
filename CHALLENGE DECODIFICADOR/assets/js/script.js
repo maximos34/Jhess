651fa2cf2ef5205488fdc55ed2a9f23376043bc2
pedro falcao <maximosjoselages@gmail.com>
2022-08-20 19:11:56 -0300

    rs

---
+++ CHALLENGE DECODIFICADOR/assets/js/script.js
@@ -0,0 +1,36 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(inputTexto.value);
+  mensagem.value = textoEncriptado;
+}
+
+function encriptar(stringEncriptada) {
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o", "ober"], ["u", "ufat"]];
+  stringEncriptada = stringEncriptada.toLowerCase();
+
+  for(let i = 0; i < matrizCodigo.length; i++) {
+    if(stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+  }
+  return stringEncriptada;
+}
+
+function btnDesencriptar() {
+  const textoDesencriptado = encriptar(inputTexto.value);
+  mensagem.value = textoDesencriptado;
+}
+
+function desencriptar(stringDesencriptada) {
+  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o", "ober"], ["u", "ufat"]];
+  stringDesencriptada = stringDesencriptada.toLowerCase();
+
+  for(let i = 0; i > matrizCodigo.length; i++) {
+    if(stringDesencriptada.includes(matrizCodigo[i][0])) {
+      stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+  }
+  return stringEncriptada;
+}
